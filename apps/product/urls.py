f141961ca50cc7abf244a9d68fad096ea6f43831
manas.dewari <manas.dewari@stylumia.com>
2023-12-17 21:35:03 +0530

    updated

--- apps/product/urls.py
+++ apps/product/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-
+from .views import ProductEntryView
 app_name = 'product'
 
 urlpatterns = [
     # path('list/', )
+    path('entry/', ProductEntryView.as_view(),name="product_entry")
 ]
--- apps/product/views.py
+++ apps/product/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 from django.views import generic
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
 
-from apps.product.model import Product
-
+from apps.product.models import Product
+from .forms import ProductForm
 
 class ProductListView(generic.ListView):
     model = Product
     template_name = 'pages/product/list.html'
     # login_url = "users:account_login"
     context_object_name = "groups"
+
+
+class ProductEntryView(CreateView):
+    model = Product
+    template_name = 'pages/product/product_entry.html'
+    form_class = ProductForm
+    success_url = reverse_lazy('product_list') 
