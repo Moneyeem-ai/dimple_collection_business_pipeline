4f9a59266ad5c6c50ec15442e068ca2d6c6657ac
Ashish Kumar Gopalika <meashishgopalika@gmail.com>
2023-12-17 14:56:47 +0530

    default template setup

--- config/urls.py
+++ config/urls.py
@@ -3,13 +3,15 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
     path('GcVVDn27GczHMeEAQeyyRShjS3WGwofrXqdFFmju0I/', admin.site.urls),
     path('', include('apps.admin_adminlte.urls')),
     path('product/', include('apps.product.urls')),
-    path('department/', include('apps.department.urls'))
+    path('department/', include('apps.department.urls')),
+    path('test/', TemplateView.as_view(template_name='pages/test.html'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
