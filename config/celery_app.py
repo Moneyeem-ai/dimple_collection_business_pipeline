38d912436ea60e98180ecaa161610f0e9e17de3d
codewithkushagra <kushagraagra008@gmail.com>
2024-01-07 12:52:51 +0530

    celery working now

---
+++ config/celery_app.py
@@ -0,0 +1,17 @@
+import time
+import os
+
+from celery import Celery, shared_task
+
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
+
+app = Celery("apps")
+app.config_from_object("django.conf:settings", namespace="CELERY")
+app.autodiscover_tasks()
+
+
+@shared_task()
+def add(x, y):
+    time.sleep(5)
+    return x + y
