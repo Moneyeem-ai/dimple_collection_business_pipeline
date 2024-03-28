25378f2b6b644e477e34f15e04d40f17e009398c
Ubuntu <ubuntu@ip-172-31-39-224.ap-south-1.compute.internal>
2024-03-28 09:50:04 +0000

    removed vevn

--- config/settings/production.py
+++ config/settings/production.py
@@ -15,7 +15,7 @@ DEBUG = False
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = [ "0.0.0.0", "16.170.221.25"]
+ALLOWED_HOSTS = [ "0.0.0.0", "pipeline.officedimple.co.in", "103.92.103.54"]
 
 # DATABASES
 # ------------------------------------------------------------------------------
