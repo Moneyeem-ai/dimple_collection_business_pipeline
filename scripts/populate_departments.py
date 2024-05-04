a527d6ca3722adb1c5e76d4ad931adac9043d4f2
codewithkushagra <kushagraagra008@gmail.com>
2024-05-04 12:35:14 +0530

    script for department upload added

---
+++ scripts/populate_departments.py
@@ -0,0 +1,14 @@
+import os
+
+import pandas as pd
+
+from config.settings.base import BASE_DIR
+from apps.department.models import Department, Category, SubCategory
+
+csv_file_path = os.path.join(BASE_DIR, 'data', 'departments_data.csv')
+df = pd.read_csv(csv_file_path)
+
+for index, row in df.iterrows():
+    department, created = Department.objects.get_or_create(department_name=row['DEPARTMENT'])
+    category, created = Category.objects.get_or_create(category_name=row['CATEGORY'], department=department)
+    sub_category, created = SubCategory.objects.get_or_create(subcategory_name=row['SUBCATEGORY'], category=category)
