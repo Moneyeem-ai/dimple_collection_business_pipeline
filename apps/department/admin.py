b7e618969d5fe07acf624a5c9aee7c541fdc0eeb
codewithkushagra <kushagraagra008@gmail.com>
2024-05-07 00:53:49 +0530

    updated admin panel

--- apps/department/admin.py
+++ apps/department/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
-from apps.department.models import Department,Category,SubCategory, Brand, Size
-# Register your models here.
-admin.site.register(Department)
-admin.site.register(Category)
-admin.site.register(SubCategory)
-admin.site.register(Brand)
-admin.site.register(Size)
+from apps.department.models import Department, Category, SubCategory, Brand, Size
+
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'department_name', 'suffix']
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'department', 'category_name', 'hsn_code', 'suffix']
+
+class SubCategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'category', 'subcategory_name', 'suffix']
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ['id', 'brand_name', 'brand_code', 'supplier_name', 'prefix']
+
+class SizeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'department', 'size_value']
+
+admin.site.register(Department, DepartmentAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(SubCategory, SubCategoryAdmin)
+admin.site.register(Brand, BrandAdmin)
+admin.site.register(Size, SizeAdmin)
--- apps/product/admin.py
+++ apps/product/admin.py
@@ -1,9 +1,23 @@
 from django.contrib import admin
 
-from .models import Product, ProductImage, PTFileEntry,ProductBarcode,PTFileBatch
+from .models import Product, ProductImage, PTFileEntry, ProductBarcode, PTFileBatch
 
-admin.site.register(Product)
-admin.site.register(ProductImage)
-admin.site.register(PTFileEntry)
-admin.site.register(ProductBarcode)
-admin.site.register(PTFileBatch)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'department', 'category', 'subcategory', 'brand', 'article_number', 'product_images', 'metadata', 'created_at', 'updated_at', 'hash_value']
+
+@admin.register(ProductImage)
+class ProductImageAdmin(admin.ModelAdmin):
+    list_display = ['id', 'product_image', 'tag_image']
+
+@admin.register(PTFileEntry)
+class PTFileEntryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'product', 'status', 'size', 'quantity', 'color', 'per_price', 'mrp', 'invoice_number', 'invoice_date', 'suffix']
+
+@admin.register(ProductBarcode)
+class ProductBarcodeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'pt_entry', 'barcode', 'sold']
+
+@admin.register(PTFileBatch)
+class PTFileBatchAdmin(admin.ModelAdmin):
+    list_display = ['id', 'batch_id', 'ptfile_entry_ids', 'is_file_uploaded', 'is_exported', 'is_image_exported']
