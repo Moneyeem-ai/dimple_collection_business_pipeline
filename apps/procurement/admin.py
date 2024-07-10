2075f4329fdd1f0fd8a09a8eb58afe1067c1f2ff
ashish-gopalika <meashishgopalika@gmail.com>
2024-07-11 00:56:35 +0530

    changes in PO create

--- apps/procurement/admin.py
+++ apps/procurement/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ProcurementOrder, ProcurementItem, ProcuredProduct, UnauthorizedProduct
+
+@admin.register(ProcurementOrder)
+class ProcurementOrderAdmin(admin.ModelAdmin):
+    list_display = ['status', 'brand', 'intent_number', 'terms_of_shipment', 'created_at', 'updated_at']
+
+@admin.register(ProcurementItem)
+class ProcurementItemAdmin(admin.ModelAdmin):
+    list_display = ['order', 'color', 'product', 'quantity_and_size', 'created_at', 'updated_at']
+
+@admin.register(ProcuredProduct)
+class ProcuredProductAdmin(admin.ModelAdmin):
+    list_display = ['po_items', 'pt_entries']
+
+@admin.register(UnauthorizedProduct)
+class UnauthorizedProductAdmin(admin.ModelAdmin):
+    list_display = ['status', 'pt_entry']
--- apps/procurement/views.py
+++ apps/procurement/views.py
@@ -1,18 +1,24 @@
 from django.views import generic
 from django.urls import reverse_lazy
 
+from apps.utils.utils import SideBarSelectedMixin
+
 from .models import ProcurementOrder
 from .forms import ProcurementOrderForm
 
 
-class ProcurementOrderCreateView(generic.CreateView):
+class ProcurementOrderCreateView(SideBarSelectedMixin, generic.CreateView):
     model = ProcurementOrder
     form_class = ProcurementOrderForm
+    parent = "procurement"
+    segment = "create_procurement_order"
     template_name = 'pages/procurement_order/create_procurement_order.html'
     success_url = reverse_lazy('procurment_order_list')
 
 
-class ProcurementOrderListView(generic.ListView):
+class ProcurementOrderListView(SideBarSelectedMixin, generic.ListView):
     model = ProcurementOrder
+    parent = "procurement"
+    segment = "procurement_order_list"
     template_name = 'pages/procurement_order/procurement_order_list.html'
     context_object_name = 'orders'
