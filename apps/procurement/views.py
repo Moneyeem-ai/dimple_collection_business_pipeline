a96edb66ae9ec27ef55a0c57e7d7597dfe0b08fe
ashish-gopalika <meashishgopalika@gmail.com>
2024-07-11 18:45:48 +0530

    form logic done

--- apps/procurement/views.py
+++ apps/procurement/views.py
@@ -7,6 +7,8 @@ from django.views import View
 
 from apps.utils.utils import SideBarSelectedMixin
 
+from apps.product.models import Product
+from apps.department.models import Department
 from .models import ProcurementOrder,ProcurementItem
 from .forms import ProcurementOrderForm
 
@@ -39,20 +41,30 @@ class ProcurementOrderView(View):
         try:
             data = json.loads(request.body)
             items = data.get('items', [])
-
             order = ProcurementOrder.objects.create(
             )
-
             for item in items:
-                ProcurementItem.objects.create(
-                    procurement_order=order,
-                    article_no=item.get('article_no'),
-                    item=item.get('item'),
-                    color=item.get('color'),
-                    quantity_and_size=item.get('quantity_and_size'),
-                    remark=item.get('remark', '')
-                )
+                article_number = item.get('article_number')
+                department_name = item.get('item')
 
+                department, created = Department.objects.get_or_create(department_name=department_name)
+
+                product = Product.objects.create(
+                    article_number=article_number,
+                    department=department,
+                    category=None,
+                    subcategory=None,
+                    brand=None,
+                    product_images=None,
+                    metadata=None,
+                    hash_value=None,
+                )
+                ProcurementItem.objects.create(
+                    order=order,
+                    color=item.get('color'),
+                    product = product,
+                    quantity_and_size=item.get('quantity_and_size')
+                )
             return JsonResponse({"status": "success", "message": "Procurement order drafted successfully."})
         except Exception as e:
             return JsonResponse({"status": "error", "message": str(e)})
